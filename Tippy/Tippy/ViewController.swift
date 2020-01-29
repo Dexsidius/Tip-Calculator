d773ca6c0999e9854d234fab7fa9768884adb769
Da'Shawn Larry <30280588+Dexsidius@users.noreply.github.com>
2020-01-28 23:48:17 -0800

    Added  Tippy Calculator

---
+++ Tippy/Tippy/ViewController.swift
@@ -0,0 +1,54 @@
+//
+//  ViewController.swift
+//  Tippy
+//
+//  Created by vagrant on 28/01/2020.
+//  Copyright © 2020 vagrant. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var billField: UITextField!
+    
+    @IBOutlet weak var tipLabel: UILabel!
+    
+    @IBOutlet weak var totalLabel: UILabel!
+    
+    @IBOutlet weak var tipControl: UISegmentedControl!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    @IBAction func calculateTip(_ sender: Any) {
+        let bill = Double(billField.text!) ?? 0
+        			
+        let tipPercentages = [0.15, 0.18, 0.2]
+        
+        let tip = bill * tipPercentages[tipControl.selectedSegmentIndex]
+        let total = tip + bill
+        
+        tipLabel.text = String(format: "$%.2f", tip)
+        totalLabel.text = String(format: "$%.2f", total)
+        
+        
+    }
+    
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    @IBAction func onTap(_ sender: Any){
+        
+        view.endEditing(true)
+    }
+    
+    
+
+
+}
+
+
